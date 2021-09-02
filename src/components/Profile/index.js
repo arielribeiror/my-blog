d11e9e1c02858776129daa26655b5987143f0e80
Ariel Ribeiro <ariiel.ribeiro@gmail.com>
2021-09-01 23:56:54 -0300

    about page added an layout component update with Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Ariel Ribeiro</h1>
+    <h2>Software Engineer</h2>
+    <p>
+      Praticamente 14 anos de experiência em frontend. Nos primeiros anos, muito
+      HTML e CSS, um pouco de JavaScript (usei muito JQuery para fazer menus e
+      animações) e até me arrisquei em PHP. Através de estudos, cursos, sites
+      desenvolvidos em freelas, temporada em outro Estado como analista de
+      negócios e empresa de marketing criada por mim, passaram-se 11 anos.
+      Agora, desde 2018, desenvolvo aplicações Angular para a Indústria 4.0.
+    </p>
+  </div>
+)
+
+export default Profile
