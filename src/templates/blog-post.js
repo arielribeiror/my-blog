1bdaf504ed2dcf7a333485e7a981745cf5327c28
Ariel Ribeiro <ariiel.ribeiro@gmail.com>
2021-11-03 08:02:23 -0300

    blog post layout template added

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Layout from "../components/Layout"
+import Seo from "../components/seo"
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return (
-    <>
+    <Layout>
+      <Seo title={post.frontmatter.title}></Seo>
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    </Layout>
   )
 }
 
